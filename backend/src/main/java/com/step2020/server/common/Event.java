e6206f5669e5d86c5c8dcd5c9f03167b03b32a19
John Kim <johnkim1108@gmail.com>
2020-06-23 19:04:26 -0400

    Add event class and attempt firebase intialization

---
+++ backend/src/main/java/com/step2020/server/common/Event.java
@@ -0,0 +1,139 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.step2020.server.common;
+
+public class Event {
+  
+  private String eventId;
+  private String eventName;
+  private String description;
+  private String location;
+  private String ownerId;
+  private String organization;
+  private String imagePath;
+  private String attendeesListId;
+
+  public static class Builder {
+
+    private String eventId;
+    private String eventName;
+    private String description;
+    private String location;
+    private String ownerId;
+    private String organization;
+    private String imagePath;
+    private String attendeesListId;
+
+    public Builder() {
+      this.eventId = "";
+      this.eventName = "";
+      this.description - "";
+      this.location = "";
+      this.ownerId = "";
+      this.organization = "";
+      this.imagePath = "";
+      this.attendeesListId = "";
+    }
+
+    public Builder withEventId(String id) {
+      this.eventId = id;
+      return this;
+    }
+
+    public Builder withName(String name) {
+      this.eventName = name;
+      return this;
+    }
+
+    public Builder withDescription(String description) {
+      this.description = description;
+      return this;
+    }
+
+    public Builder atLocation(String location) {
+      this.location = location;
+      return this;
+    }
+
+    public Builder withOwnerId(String id) {
+      this.ownerId = id;
+      return this;
+    }
+
+    public Builder withOrganization(String org) {
+      this.organization = org;
+      return this;
+    }
+
+    public Builder withImagePath(String path) {
+      this.imagePath = path;
+      return this;
+    }
+
+    public Builder withAttendeesListId(String id) {
+      this.attendeesListId = id;
+      return this;
+    }
+
+    public Event build() {
+      Event event = new Event();
+      event.eventId = this.eventId;
+      event.evenName = this.eventName;
+      event.description = this.description;
+      event.location = this.location;
+      event.ownerId = this.ownerId;
+      event.organization = this.organization;
+      event.imagePath = this.imagePath;
+      event.attendeesListId = this.attendeesListId;
+
+      return event;
+    }
+
+  }
+  
+  public Event() {}
+
+  public String getEventId() {
+    return this.eventId;
+  }
+
+  public String getEventName() {
+    return this.eventName;
+  }
+
+  public String getDescription() {
+    return this.description;
+  }
+
+  public String getLocation() {
+    return this.location;
+  }
+
+  public String getOwnerId() {
+    return this.ownerId;
+  }
+
+  public String getOrganization() {
+    return this.organization;
+  }
+
+  public String getImagePath() {
+    return this.imagePath;
+  }
+
+  public String getAtttendeesListId() {
+    return this.attendeesListId;
+  }
+}
