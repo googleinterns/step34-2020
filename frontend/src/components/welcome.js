2d8c4df44873c24789621bc9e6ba544fef1302f9
Maya <mayahp@seas.upenn.edu>
2020-06-26 16:58:31 +0000

    indent fixes

--- frontend/src/components/welcome.js
+++ frontend/src/components/welcome.js
@@ -3,67 +3,61 @@ import Script from 'react-load-script';
 import { Form, Button, Jumbotron } from 'react-bootstrap'
 
 class Search extends Component {
-  
-	// Define Constructor
-	constructor(props) {
-    	super(props);
-        this.handleScriptLoad = this.handleScriptLoad.bind(this);
 
-    	// Declare State
-    	this.state = {
-    		city: '',
-    		query: ''
-    	};
+  // Define Constructor
+  constructor(props) {
+    super(props);
+    this.handleScriptLoad = this.handleScriptLoad.bind(this);
 
-	}
+    // Declare State
+    this.state = {
+      city: '',
+      query: ''
+    };
 
-  	render() {
-    	return (
-      	    <div>
-    	    	<Script url="https://maps.googleapis.com/maps/api/js?key=KEY&libraries=places" onLoad={this.handleScriptLoad}/>
-                <Jumbotron>
-                	<h1>Welcome to MapIT!</h1>
-                	<Form>
-                		<Form.Group controlId="universityForm">
-                    		<Form.Label>Enter your university</Form.Label>
-                        	<br/>
-                        	<Form.Control id="autocomplete" placeholder="Enter university" />
-                    	</Form.Group>
-                    	<br/>
-                    	<Button variant="primary" type="submit">
-                    		Find Events
-                    	</Button>
-                	</Form>
-                </Jumbotron>
-     		</div>
-        );
-  	}
+  }
 
-    handleScriptLoad() { 
-    	// Declare Options For Autocomplete 
+  render() {
+    return ( 
+      <div>
+        <Script url = "https://maps.googleapis.com/maps/api/js?key=KEY&libraries=places" onLoad = {this.handleScriptLoad}/> 
+        <Jumbotron >
+          <h1> Welcome to MapIT! </h1> 
+          <Form>
+            <Form.Group controlId = "universityForm">
+            <Form.Label> Enter your university </Form.Label> 
+            <br/>
+            <Form.Control id = "autocomplete" placeholder = "Enter university"/>
+            </Form.Group> 
+            <br/>
+            <Button variant = "primary" type = "submit">Find Events</Button>
+          </Form> 
+        </Jumbotron>
+      </div>
+    );
+  }
 
-        const types = ["university"];
-  		// Initialize Google Autocomplete 
-  		/*global google*/
-    	var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'), types);
-  
-  		autocomplete.setFields(['address_components', 'formatted_address']);
-  		autocomplete.addListener('place-changed', this.handlePlaceSelect); 
-	}
+  handleScriptLoad() {
+    const types = ["university"];
 
-    handlePlaceSelect() {
-        const addressObject = this.autocomplete.getPlace();
-        const address = addressObject.address_components;
+    /*global google*/
+    var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'), types);
 
-        if (address) {
-            this.setState(
-                {
-                    city: address[0].long_name,
-                    query: addressObject.formatted_address,
-                }
-            );
-        }
+    autocomplete.setFields(['address_components', 'formatted_address']);
+    autocomplete.addListener('place-changed', this.handlePlaceSelect);
+  }
+
+  handlePlaceSelect() {
+    const addressObject = this.autocomplete.getPlace();
+    const address = addressObject.address_components;
+
+    if (address) {
+      this.setState({
+        city: address[0].long_name,
+        query: addressObject.formatted_address,
+      });
     }
+  }
 }
 
 export default Search;
