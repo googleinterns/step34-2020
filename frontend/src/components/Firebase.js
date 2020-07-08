a7c4b92be6cb61effc43dce54ea549159e07b796
Mico <micomyiza@google.com>
2020-07-08 11:51:47 -0400

    resolve styling issues

--- frontend/src/components/Firebase.js
+++ frontend/src/components/Firebase.js
@@ -30,44 +30,44 @@ class Firebase {
     if (!firebase.apps.length) {
       // Check if we are running on development or production
       if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-	// Development	
-	// Make the default app the config
-	this.defaultApp = firebase.initializeApp(testConfig);
-	
-	// Make the users app with a different database url
-	this.usersApp = firebase.initializeApp({ 
-	  databaseURL: "https://step-34-2020-test.firebaseio.com/"
-	}, 'users-app');
+  // Development	
+  // Make the default app the config
+  this.defaultApp = firebase.initializeApp(testConfig);
+  
+  // Make the users app with a different database url
+  this.usersApp = firebase.initializeApp({ 
+    databaseURL: "https://step-34-2020-test.firebaseio.com/"
+  }, 'users-app');
 
-	// Make the events app with a different database url
-	this.eventsApp = firebase.initializeApp({ 
-	  databaseURL: "https://step-34-2020-test.firebaseio.com/"
-	}, 'events-app');
+  // Make the events app with a different database url
+  this.eventsApp = firebase.initializeApp({ 
+    databaseURL: "https://step-34-2020-test.firebaseio.com/"
+  }, 'events-app');
 
-	// Get references from each app
-	this.sessionsRef = firebase.database();
-	this.userRef = this.usersApp.database().ref('user-info/');
-	this.eventsRef =  this.eventsApp.database().ref('events/');
+  // Get references from each app
+  this.sessionsRef = firebase.database();
+  this.userRef = this.usersApp.database().ref('user-info/');
+  this.eventsRef =  this.eventsApp.database().ref('events/');
       
       } else {
-	// Production
-	// Make the default app the config
-	this.defaultApp = firebase.initializeApp(config);
-	
-	// Make the users app with a different database url
-	this.usersApp = firebase.initializeApp({ 
-	  databaseURL: "https://step-34-2020-user-info.firebaseio.com/"
-	}, 'users-app');
+  // Production
+  // Make the default app the config
+  this.defaultApp = firebase.initializeApp(config);
+  
+  // Make the users app with a different database url
+  this.usersApp = firebase.initializeApp({ 
+    databaseURL: "https://step-34-2020-user-info.firebaseio.com/"
+  }, 'users-app');
 
-	// Make the events app with a different database url
-	this.eventsApp = firebase.initializeApp({ 
-	  databaseURL: "https://step-34-2020-events.firebaseio.com/"
-	}, 'events-app');
+  // Make the events app with a different database url
+  this.eventsApp = firebase.initializeApp({ 
+    databaseURL: "https://step-34-2020-events.firebaseio.com/"
+  }, 'events-app');
 
-	// Get references from each app
-	this.sessionsRef = firebase.database();
-	this.userRef = this.usersApp.database().ref();
-	this.eventsRef =  this.eventsApp.database().ref();
+  // Get references from each app
+  this.sessionsRef = firebase.database();
+  this.userRef = this.usersApp.database().ref();
+  this.eventsRef =  this.eventsApp.database().ref();
       }
 
       // Start the session
@@ -136,19 +136,19 @@ class Firebase {
     // Listen for responses under the RESPONSES path
     var listener = ref.ref('RESPONSES').child(sessionId).on('child_added', function(snapshot) {
       if (snapshot.key === requestId) {
-	// Get the status and message
-	var status = snapshot.child("status").val();
-	var message = snapshot.child("message").val();
-	console.log(status);
-	// When the status is "success" sign in and make deferred promise true
-	if (status === "success") {
-	  firebase.auth().signInWithEmailAndPassword(email, password);
-	  deferred.resolve(true);
-	// When the status is "failed" show error message and deferred promise as false
-	} else { 
-	  alert(message);
-	  deferred.resolve(false);
-	}
+  // Get the status and message
+  var status = snapshot.child("status").val();
+  var message = snapshot.child("message").val();
+  console.log(status);
+  // When the status is "success" sign in and make deferred promise true
+  if (status === "success") {
+    firebase.auth().signInWithEmailAndPassword(email, password);
+    deferred.resolve(true);
+  // When the status is "failed" show error message and deferred promise as false
+  } else { 
+    alert(message);
+    deferred.resolve(false);
+  }
       }
       // Remove the listener from this path
       ref.ref('RESPONSES').child(sessionId).off('child_added', listener);
