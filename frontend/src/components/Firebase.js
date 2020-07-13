bc385deb44be0119232e268b0ff46c6b126b359f
John Kim <johnkim1108@gmail.com>
2020-07-13 19:02:10 -0400

    Update Firebase.js
    Fix indentations

--- frontend/src/components/Firebase.js
+++ frontend/src/components/Firebase.js
@@ -245,10 +245,10 @@ class Firebase {
       console.log(path);
       // If a url is null then return
       if (path == null) {
-	      return;
+	return;
       } else {
-				// add url to array of urls
-	      paths[i] = path;
+	// add url to array of urls
+	paths[i] = path;
       }
     }
     return paths;
@@ -262,29 +262,29 @@ class Firebase {
       var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
       console.log('Upload is ' + progress + '% done');
       switch (snapshot.state) {
-	      case firebase.storage.TaskState.PAUSED: // or 'paused'
-	        console.log('Upload is paused');
-	        break;
-	      case firebase.storage.TaskState.RUNNING: // or 'running'
-	        console.log('Upload is running');
-	        break;
+	case firebase.storage.TaskState.PAUSED: // or 'paused'
+	  console.log('Upload is paused');
+	  break;
+	case firebase.storage.TaskState.RUNNING: // or 'running'
+	  console.log('Upload is running');
+	  break;
       }
     }, function(error) {
       // Handle unsuccessful uploads
       switch (error.code) {
-	      case 'storage/unauthorized':
-	        // User doesn't have permission to access the object
-	        alert("Don't have permission to upload images, are you logged in? Cancelling event creation...");
-	        break;
-	      case 'storage/canceled':
-	        // User canceled the upload
-	        alert("Cancelled upload. Cancelling event creation...");
-	        break;
-	      case 'storage/unknown':
-	        // Unknown error occurred, inspect error.serverResponse
-	        alert("Unknown error. Cancelling event creation...");
-	        console.log(error.serverResponse);
-	        break;
+	case 'storage/unauthorized':
+	  // User doesn't have permission to access the object
+	  alert("Don't have permission to upload images, are you logged in? Cancelling event creation...");
+	  break;
+	case 'storage/canceled':
+	  // User canceled the upload
+	  alert("Cancelled upload. Cancelling event creation...");
+	  break;
+	case 'storage/unknown':
+	  // Unknown error occurred, inspect error.serverResponse
+	  alert("Unknown error. Cancelling event creation...");
+	  console.log(error.serverResponse);
+	  break;
       }
       deferred.resolve(null);
     }, function() {
