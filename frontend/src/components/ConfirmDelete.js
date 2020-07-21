55ee5bca0a4d6af731390833eb6f5defff972ef5
Mico <micomyiza@google.com>
2020-07-21 13:22:14 -0400

    fix identation issues

--- frontend/src/components/ConfirmDelete.js
+++ frontend/src/components/ConfirmDelete.js
@@ -17,28 +17,28 @@ export default class ConfirmDelete extends Component {
   render() {
     return(
       <Modal
-      {...this.props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered>
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-        WARNING: This event will be deleted permenantly
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <p>Please confirm you want to delete this event.<br />
-          All data associated with this event will be delete.<br />
-          <br />
-        </p>
-        <h5>Note:</h5>
-        <p>You cannot undo the changes after you select confirm</p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="success" onClick={this.handleDelete.bind(this)}>Confirm</Button>
-        <Button variant="danger" onClick={this.props.onHide}>Cancel</Button>
-      </Modal.Footer>
-    </Modal>
+        {...this.props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered>
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+          WARNING: This event will be deleted permenantly
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Please confirm you want to delete this event.<br />
+            All data associated with this event will be delete.<br />
+            <br />
+          </p>
+          <h5>Note:</h5>
+          <p>You cannot undo the changes after you select confirm</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="success" onClick={this.handleDelete.bind(this)}>Confirm</Button>
+          <Button variant="danger" onClick={this.props.onHide}>Cancel</Button>
+        </Modal.Footer>
+      </Modal>
     )
   }
 }
